5721f55aa135513be3b07ec34d66bbd2bcba59e7
Nicolás de la Torre <7477124+esteganobvio@users.noreply.github.com>
2022-01-21 15:26:20 -0300

    more fixes

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,4 +6,8 @@ require("_toggleterm")
 require("_autopairs")
 --require("_statusline")
 
-vim.cmd("colorscheme nord")
+vim.g.nord_contrast = true
+vim.g.nord_borders = true
+vim.g.nord_disable_background = true
+vim.g.nord_italic = false
+require('nord').set()
