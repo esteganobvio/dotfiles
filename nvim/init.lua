fd412c3c8721ccf70772fe496a21e8cd983a0afa
Nicolás de la Torre <7477124+esteganobvio@users.noreply.github.com>
2022-01-31 13:23:48 -0300

    updated lsp config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,10 @@
-require("_options")
-require("_plugins")
-require("_lsp")
-require("_whichkey")
-require("_toggleterm")
-require("_autopairs")
---require("_statusline")
-
+require "_options"
+require "_plugins"
+require "user.lsp"
+require "_whichkey"
+require "_toggleterm"
+require "_autopairs"
+require "_nvim-tree"
 vim.g.nord_contrast = true
 vim.g.nord_borders = true
 vim.g.nord_disable_background = true
--- nvim/lua/_plugins.lua
+++ nvim/lua/_plugins.lua
@@ -24,25 +24,14 @@ return require("packer").startup(function()
 	})
   use({
       "folke/which-key.nvim",
-      config = function()
-          require("which-key").setup({})
-      end,
   })
 	use({
 		"kyazdani42/nvim-tree.lua",
 		requires = "kyazdani42/nvim-web-devicons",
-		config = function()
-			require("nvim-tree").setup({
-				diagnostics = { enable = true },
-				filters = {
-					dotfile = false,
-					custom = { ".git", "node_modules", ".cache" },
-				},
-			})
-		end,
 	})
 	-- lsp-config
 	use("neovim/nvim-lspconfig")
+  use("williamboman/nvim-lsp-installer")
 	--use({ "jose-elias-alvarez/null-ls.nvim", requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" } })
 	-- Nord colorscheme
 	use("shaunsingh/nord.nvim")
@@ -50,81 +39,12 @@ return require("packer").startup(function()
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-				ignore_install = { "" }, -- List of parsers to ignore installing
-				highlight = {
-					enable = true, -- false will disable the whole extension
-					disable = { "" }, -- list of language that will be disabled
-					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-					-- Using this option may slow down your editor, and you may see some duplicate highlights.
-					-- Instead of true it can also be a list of languages
-					additional_vim_regex_highlighting = false,
-				},
-			})
-		end,
 	})
 
 	use("L3MON4D3/LuaSnip") -- Snippets plugin
 
 	use({
 		"hrsh7th/nvim-cmp",
-		config = function()
-			local luasnip = require("luasnip")
-			local cmp = require("cmp")
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				mapping = {
-					["<C-p>"] = cmp.mapping.select_prev_item(),
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					["<C-d>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.close(),
-					["<CR>"] = cmp.mapping.confirm({
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
-					}),
-					["<Tab>"] = function(fallback)
-						if vim.fn.pumvisible() == 1 then
-							vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
-						elseif luasnip.expand_or_jumpable() then
-							vim.fn.feedkeys(
-								vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
-								""
-							)
-						else
-							fallback()
-						end
-					end,
-					["<S-Tab>"] = function(fallback)
-						if vim.fn.pumvisible() == 1 then
-							vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
-						elseif luasnip.jumpable(-1) then
-							vim.fn.feedkeys(
-								vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true),
-								""
-							)
-						else
-							fallback()
-						end
-					end,
-				},
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
-					{ name = "rg" },
-					{ name = "path" },
-				},
-			})
-		end,
 	})
 
 	use("hrsh7th/cmp-buffer")
@@ -137,71 +57,11 @@ return require("packer").startup(function()
 	use({
 		"hoob3rt/lualine.nvim",
 		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-		config = function()
-			require("lualine").setup({
-				options = {
-					icons_enabled = false,
-					theme = "nord",
-					component_separators = { "", "" },
-					section_separators = { "", "" },
-					disabled_filetypes = {},
-				},
-				sections = {
-					lualine_a = { "mode" },
-					lualine_b = { "branch" },
-					lualine_c = {
-						{
-							"filename",
-							file_status = true,
-							path = 1,
-						},
-						{
-							"diagnostics",
-							-- table of diagnostic sources, available sources:
-							-- nvim_lsp, coc, ale, vim_lsp
-							sources = { "nvim_diagnostic" },
-							-- displays diagnostics from defined severity
-							sections = { "error", "warn", "info", "hint" },
-							-- all colors are in format #rrggbb
-							symbols = { error = "E", warn = "W", info = "I", hint = "H" },
-						},
-					},
-					lualine_x = { "encoding", "fileformat", "filetype" },
-					lualine_y = { "progress" },
-					lualine_z = { "location" },
-				},
-				inactive_sections = {
-					lualine_a = {},
-					lualine_b = {},
-					lualine_c = {
-						{
-							"filename",
-							file_status = true,
-							path = 1,
-						},
-					},
-					lualine_x = { "location" },
-					lualine_y = {},
-					lualine_z = {},
-				},
-				tabline = {},
-				extensions = {
-					"nvim-tree",
-				},
-			})
-		end,
 	})
 
 	use({
 		"folke/trouble.nvim",
 		requires = "kyazdani42/nvim-web-devicons",
-		config = function()
-			require("trouble").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			})
-		end,
 	})
 
 	use({ "akinsho/toggleterm.nvim" })
