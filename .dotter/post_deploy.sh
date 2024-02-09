d5f6effc76816c70cc798430b7829be4883b866e
Nicolás de la Torre <7477124+esteganobvio@users.noreply.github.com>
2024-02-09 16:58:12 -0300

    super update

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -1,3 +1,7 @@
 {{#if dotter.packages.systemd-user}}
 systemctl enable --now --user gnome-keyring-daemon
 {{/if}}
+{{#if dotter.packages.waybar}}
+killall waybar
+waybar &
+{{/if}}
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
-  'nvim-lualine/lualine.nvim',
+  "nvim-lualine/lualine.nvim",
   dependencies = {
-    'nvim-tree/nvim-web-devicons'
+    "nvim-tree/nvim-web-devicons",
   },
   config = function()
     require("lualine").setup({
@@ -9,47 +9,49 @@ return {
         icons_enabled = true,
       },
       extensions = {
-        'toggleterm',
-        'nvim-tree',
-        'trouble'
+        "toggleterm",
+        "nvim-tree",
+        "trouble",
       },
       sections = {
         lualine_c = {
           {
-            'filename',
+            "filename",
             path = 1,
           },
         },
         lualine_x = {
           {
-            'copilot',
+            "copilot",
             symbols = {
-                status = {
-                    icons = {
-                        enabled = "",
-                        disabled = "",
-                        warning = "",
-                        unknown = ""
-                    },
-                    hl = {
-                        enabled = "#50FA7B",
-                        disabled = "#6272A4",
-                        warning = "#FFB86C",
-                        unknown = "#FF5555"
-                    }
+              status = {
+                icons = {
+                  enabled = "",
+                  disabled = "",
+                  sleep = "",
+                  warning = "",
+                  unknown = "",
                 },
-                spinners = require("copilot-lualine.spinners").dots,
-                spinner_color = "#6272A4"
+                hl = {
+                  enabled = "#50FA7B", -- green
+                  disabled = "#6272A4", -- blue
+                  sleep = "#AEB7D0", -- grey
+                  warning = "#FFB86C", -- yellow
+                  unknown = "#FF5555", -- red
+                }
+              },
+              spinners = require("copilot-lualine.spinners").dots,
+              spinner_color = "#6272A4", -- blue
             },
 
             show_colors = true,
-            show_loading = true
+            show_loading = true,
           },
-          'encoding',
-          'fileformat',
-          'filetype',
+          "encoding",
+          "fileformat",
+          "filetype",
         }, -- I added copilot here
       },
     })
-  end
+  end,
 }
