48d8be1309310aa1cb5bb3ffd80a0c03f57abf65
Nicolás de la Torre <7477124+esteganobvio@users.noreply.github.com>
2025-04-21 23:55:03 -0300

    add tpm install

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -5,3 +5,9 @@ fi
 if [ ! -d "$HOME"/.oh-my-zsh ]; then
   git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME"/.oh-my-zsh
 fi
+
+if [ ! -d "$HOME"/.tmux/plugins/tpm ]; then
+  git clone https://github.com/tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm
+fi
+
+
