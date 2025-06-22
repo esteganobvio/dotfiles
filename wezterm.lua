e09f4c3e528c69f8eddcdae12787639be20f673c
Nicolás de la Torre <7477124+esteganobvio@users.noreply.github.com>
2025-06-22 19:51:34 -0300

    update niri config update wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -3,12 +3,12 @@ local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smar
 local sessionizer = wezterm.plugin.require("https://github.com/ElCapitanSponge/sessionizer.wezterm")
 local config = wezterm.config_builder()
 -- =>
-config.default_prog = { '/bin/zsh', '-l' }
+config.default_prog = { "/bin/zsh", "-l" }
 config.font = wezterm.font("{{ font_family }}")
 config.font_size = 10
 config.line_height = 1.0
 config.color_scheme = "tokyonight"
-config.window_background_opacity = 0.9
+config.window_background_opacity = 1
 config.window_decorations = "TITLE|RESIZE"
 -- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.animation_fps = 60
@@ -17,9 +17,63 @@ config.integrated_title_buttons = { "Close" }
 config.enable_wayland = true
 -- config.front_end = "WebGpu"
 -- config.disable_default_key_bindings = true
+config.hide_tab_bar_if_only_one_tab = true
 config.window_frame = {
 	font = wezterm.font({ family = "{{ font_family }}" }),
 	font_size = 10,
+	-- The overall background color of the tab bar when
+	-- the window is focused
+	active_titlebar_bg = "#1a1b26",
+
+	-- The overall background color of the tab bar when
+	-- the window is not focused
+	inactive_titlebar_bg = "#1a1b26",
+}
+
+config.colors = {
+	tab_bar = {
+		background = "#1a1b26",
+		active_tab = {
+			-- The color of the background area for the tab
+			bg_color = "#7aa2f7",
+			-- The color of the text for the tab
+			fg_color = "#16161e",
+		},
+		inactive_tab = {
+			bg_color = "#292e42",
+			fg_color = "#545c7e",
+
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `inactive_tab`.
+		},
+		-- You can configure some alternate styling when the mouse pointer
+		-- moves over inactive tabs
+		inactive_tab_hover = {
+			bg_color = "#292e42",
+			fg_color = "#7aa2f7",
+			italic = true,
+
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `inactive_tab_hover`.
+		},
+		new_tab = {
+			bg_color = "#7aa2f7",
+			fg_color = "#1a1b26",
+
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `new_tab`.
+		},
+		new_tab_hover = {
+			bg_color = "#1a1b26",
+			fg_color = "#7aa2f7",
+			italic = true,
+
+			-- The same options that were listed under the `active_tab` section above
+			-- can also be used for `new_tab_hover`.
+		},
+		-- The color of the inactive tab bar edge/divider
+		inactive_tab_edge = "#16161e",
+	},
 }
 
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
