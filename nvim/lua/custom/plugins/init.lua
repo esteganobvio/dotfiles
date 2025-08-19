e118a6723e3514b5d9b885f231593a7fc436f9ed
Nicolás de la Torre <7477124+esteganobvio@users.noreply.github.com>
2025-08-19 20:09:44 -0300

    disable avante

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -47,99 +47,99 @@ return {
       'folke/snacks.nvim', -- optional
     },
   },
-  {
-    'yetone/avante.nvim',
-    event = 'VeryLazy',
-    version = false, -- Never set this value to "*"! Never!
-    opts = {
-      -- add any opts here
-      -- for example
-      provider = 'gemini',
-      providers = {
-        gemini = {
-          endpoint = 'https://generativelanguage.googleapis.com/v1beta/models',
-          model = 'gemini-2.5-flash',
-          timeout = 30000, -- Timeout in milliseconds
-          extra_body_requests = {
-            temperature = 0,
-            max_tokens = 8192,
-          },
-        },
-        ollama = {
-          endpoint = 'http://127.0.0.1:11434',
-          model = 'gemma3:12b',
-          timeout = 30000, -- Timeout in millisecondsu can disable tools by setti
-          disable_tools = { 'web_search' },
-          extra_request_body = {
-            options = {
-              temperature = 0.75,
-              num_ctx = 20480,
-              keep_alive = '5m',
-            },
-          },
-        },
-      },
-      rag_service = {
-        enabled = false, -- Enables the RAG service
-        host_mount = os.getenv 'HOME', -- Host mount path for the rag service (Docker will mount this path)
-        runner = 'docker', -- Runner for the RAG service (can use docker or nix)
-        llm = { -- Language Model (LLM) configuration for RAG service
-          provider = 'ollama', -- LLM provider
-          endpoint = 'http://127.0.0.1:11434', -- LLM API endpoint
-          api_key = '', -- Environment variable name for the LLM API key
-          model = 'gemma3:12b', -- LLM model name
-          extra = nil, -- Additional configuration options for LLM
-        },
-        embed = { -- Embedding model configuration for RAG service
-          provider = 'ollama', -- Embedding provider
-          endpoint = 'http://127.0.0.1:11434', -- Embedding API endpoint
-          api_key = '', -- Environment variable name for the embedding API key
-          model = 'mxbai-embed-large:latest', -- Embedding model name
-          extra = nil, -- Additional configuration options for the embedding model
-        },
-        docker_extra_args = '', -- Extra arguments to pass to the docker command
-      },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = 'make',
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'stevearc/dressing.nvim',
-      'nvim-lua/plenary.nvim',
-      'MunifTanjim/nui.nvim',
-      --- The below dependencies are optional,
-      'echasnovski/mini.pick', -- for file_selector provider mini.pick
-      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
-      'ibhagwan/fzf-lua', -- for file_selector provider fzf
-      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-      {
-        -- support for image pasting
-        'HakonHarnes/img-clip.nvim',
-        event = 'VeryLazy',
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { 'markdown', 'Avante' },
-        },
-        ft = { 'markdown', 'Avante' },
-      },
-    },
-  },
+  --  {
+  --    'yetone/avante.nvim',
+  --    event = 'VeryLazy',
+  --    version = false, -- Never set this value to "*"! Never!
+  --    opts = {
+  --      -- add any opts here
+  --      -- for example
+  --      provider = 'ollama',
+  --      providers = {
+  --        gemini = {
+  --          endpoint = 'https://generativelanguage.googleapis.com/v1beta/models',
+  --          model = 'gemini-2.5-flash',
+  --          timeout = 30000, -- Timeout in milliseconds
+  --          extra_body_requests = {
+  --            temperature = 0,
+  --            max_tokens = 8192,
+  --          },
+  --        },
+  --        ollama = {
+  --          endpoint = 'http://127.0.0.1:11434',
+  --          model = 'gemma3:12b',
+  --          timeout = 30000, -- Timeout in millisecondsu can disable tools by setti
+  --          disable_tools = { 'web_search' },
+  --          extra_request_body = {
+  --            options = {
+  --              temperature = 0.75,
+  --              num_ctx = 20480,
+  --              keep_alive = '5m',
+  --            },
+  --          },
+  --        },
+  --      },
+  --      rag_service = {
+  --        enabled = false, -- Enables the RAG service
+  --        host_mount = os.getenv 'HOME', -- Host mount path for the rag service (Docker will mount this path)
+  --        runner = 'docker', -- Runner for the RAG service (can use docker or nix)
+  --        llm = { -- Language Model (LLM) configuration for RAG service
+  --          provider = 'ollama', -- LLM provider
+  --          endpoint = 'http://127.0.0.1:11434', -- LLM API endpoint
+  --          api_key = '', -- Environment variable name for the LLM API key
+  --          model = 'gemma3:12b', -- LLM model name
+  --          extra = nil, -- Additional configuration options for LLM
+  --        },
+  --        embed = { -- Embedding model configuration for RAG service
+  --          provider = 'ollama', -- Embedding provider
+  --          endpoint = 'http://127.0.0.1:11434', -- Embedding API endpoint
+  --          api_key = '', -- Environment variable name for the embedding API key
+  --          model = 'mxbai-embed-large:latest', -- Embedding model name
+  --          extra = nil, -- Additional configuration options for the embedding model
+  --        },
+  --        docker_extra_args = '', -- Extra arguments to pass to the docker command
+  --      },
+  --    },
+  --    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --    build = 'make',
+  --    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  --    dependencies = {
+  --      'nvim-treesitter/nvim-treesitter',
+  --      'stevearc/dressing.nvim',
+  --      'nvim-lua/plenary.nvim',
+  --      'MunifTanjim/nui.nvim',
+  --      --- The below dependencies are optional,
+  --      'echasnovski/mini.pick', -- for file_selector provider mini.pick
+  --      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+  --      'ibhagwan/fzf-lua', -- for file_selector provider fzf
+  --      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+  --      {
+  --        -- support for image pasting
+  --        'HakonHarnes/img-clip.nvim',
+  --        event = 'VeryLazy',
+  --        opts = {
+  --          -- recommended settings
+  --          default = {
+  --            embed_image_as_base64 = false,
+  --            prompt_for_file_name = false,
+  --            drag_and_drop = {
+  --              insert_mode = true,
+  --            },
+  --            -- required for Windows users
+  --            use_absolute_path = true,
+  --          },
+  --        },
+  --      },
+  --      {
+  --        -- Make sure to set this up properly if you have lazy=true
+  --        'MeanderingProgrammer/render-markdown.nvim',
+  --        opts = {
+  --          file_types = { 'markdown', 'Avante' },
+  --        },
+  --        ft = { 'markdown', 'Avante' },
+  --      },
+  --    },
+  --  },
   {
     'linux-cultist/venv-selector.nvim',
     dependencies = {
